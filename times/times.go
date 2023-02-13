e588740d4085703c29a5137b3eab05e5bcf7e6f7
toddlerya <toddlerya@gmail.com>
2023-02-13 16:30:22 +0800

    增加解析时间字符串方法

--- times/times.go
+++ times/times.go
@@ -6,3 +6,11 @@ import "time"
 func NowDateTimeString() string {
 	return time.Now().Format("20060102_150405")
 }
+
+func ParseTimeWithLocation(layout, timeStr, localName string) (time.Time, error) {
+	if local, err := time.LoadLocation(localName); err != nil {
+		return time.Time{}, err
+	} else {
+		return time.ParseInLocation(layout, timeStr, local)
+	}
+}
