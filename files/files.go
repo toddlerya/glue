3c868b7bc82d27ac1947b30be7927f19c49ab13e
toddlerya <toddlerya@gmail.com>
2023-05-11 11:18:12 +0800

    文件目录拷贝区分GOOS

--- files/files.go
+++ files/files.go
@@ -14,7 +14,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"syscall"
 )
 
 // 获取文件的MD5值
@@ -89,58 +88,6 @@ func SearchFileInPath(dirPath string, fileNameGlobReg string) ([]string, error) 
 		}
 	}
 	return goalFileArrary, err
-}
-
-// 拷贝文件夹
-func CopyDirectory(scrDir, dstDir string) error {
-	entries, err := os.ReadDir(scrDir)
-	if err != nil {
-		return err
-	}
-	for _, entry := range entries {
-		sourcePath := filepath.Join(scrDir, entry.Name())
-		destPath := filepath.Join(dstDir, entry.Name())
-
-		fileInfo, err := os.Stat(sourcePath)
-		if err != nil {
-			return err
-		}
-
-		stat, ok := fileInfo.Sys().(*syscall.Stat_t)
-		if !ok {
-			return fmt.Errorf("failed to get raw syscall.Stat_t data for '%s'", sourcePath)
-		}
-
-		switch fileInfo.Mode() & os.ModeType {
-		case os.ModeDir:
-			if err := CreateDirIfNotExist(destPath, fs.ModePerm); err != nil {
-				return err
-			}
-			if err := CopyDirectory(sourcePath, destPath); err != nil {
-				return err
-			}
-		case os.ModeSymlink:
-			if err := CopySymLink(sourcePath, destPath); err != nil {
-				return err
-			}
-		default:
-			if err := Copy(sourcePath, destPath); err != nil {
-				return err
-			}
-		}
-
-		if err := os.Lchown(destPath, int(stat.Uid), int(stat.Gid)); err != nil {
-			return err
-		}
-
-		isSymlink := entry.Type()&os.ModeSymlink != 0
-		if !isSymlink {
-			if err := os.Chmod(destPath, entry.Type()); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
 
 // 拷贝文件
